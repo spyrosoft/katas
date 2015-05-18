b894b7c49a4d7a765eee762c39369d82a2b556db
Bennett Roesch <meta@spyrosoft.com>
2015-05-18 13:51:01 -0700

    Modify test.zsh to run 1.lisp.

--- 100-doors/common-lisp/test.zsh
+++ 100-doors/common-lisp/test.zsh
@@ -1,10 +1,8 @@
 #!/usr/bin/zsh
 
-#sbcl --script 2.lisp
-sbcl --script 3.lisp
+sbcl --script 1.lisp
 
 while read user_input
 do
-#	sbcl --script 2.lisp
-	sbcl --script 3.lisp
+	sbcl --script 1.lisp
 done
